9bf13172b0bf7f7337f98b3ebd69ec31334c190f
Marco Steiner <mail@steinermarco.de>
2020-08-06 15:03:40 +0200

    no message

--- gridsome.config.js
+++ gridsome.config.js
@@ -27,7 +27,7 @@ module.exports = {
           },
           {
             typeName: "Timetableblock",
-            route: '/wp/v2/timetableblock',
+            route: '/wp/v2/timetableblock?order=asc',
             normalize: true,
           },
           {
